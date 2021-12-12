2f58333c5a73c2709596aab68386314c5d3090da
skymin <ppt00003333@gmail.com>
2021-12-12 16:40:05 +0900

    update to 2.4.1

--- src/skymin/InventoryLib/InvLibManager.php
+++ src/skymin/InventoryLib/InvLibManager.php
@@ -39,7 +39,19 @@ final class InvLibManager{
 	public static function register(Plugin $plugin) :void{
 		if(self::$scheduler === null){
 			self::$scheduler = $plugin->getScheduler();
-			$plugin->getServer()->getPluginManager()->registerEvents(new InvLibEventListener(), $plugin);
+			Server::getInstance()->getPluginManager()->registerEvent(InventoryTransactionEvent::class, function(InventoryTransactionEvent $ev) : void{
+				$transaction = $ev->getTransaction();
+				foreach($transaction->getActions() as $action){
+					if(!$action instanceof SlotChangeAction) continue;
+					$inventory = $action->getInventory();
+					if(!$inventory instanceof LibInventory) continue;
+					(function() use($transaction, $action, $ev){
+						if($this->onActionSenssor(new InvLibAction($transaction->getSource(), $action->getSlot(), $action->getSourceItem(), $action->getTargetItem()))){
+							$ev->cancel();
+						}
+					})->call($inventory);
+				}
+			}, EventPriority::NORMAL, $plugin, true);
 		}
 	}
 	
